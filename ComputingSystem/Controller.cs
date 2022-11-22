2eadcb5a6b4bf7e9522c467db4c0f6b20c8b5f63
contrub <572519065z@gmail.com>
2022-11-22 12:24:55 +0200

    make Execute method static

--- ComputingSystem/Controller.cs
+++ ComputingSystem/Controller.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ComputingSystem
+﻿namespace ComputingSystem
 {
     class Controller
     {
-        public void Execute(ModelOperations operation, Model model)
+        public static void Execute(ModelOperations operation, Model model)
         {
             if (model == null)
                 throw new ArgumentNullException("Empty model");
--- ComputingSystem/View.cs
+++ ComputingSystem/View.cs
@@ -1,29 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ComputingSystem
+﻿namespace ComputingSystem
 {
     abstract class View: IDisposable
     {
         public View(Model model, Controller controller)
         {
             this.model = model;
-            this.controller = controller;
+            // this.controller = controller;
         }
 
         public readonly Model model;
-        public Controller controller
-        {
-            private get;
-            set;
-        }
+        // public Controller Controller
+        // { private get; set; }
 
         public void ReactToUserActions(ModelOperations modelOperation)
         {
-            controller.Execute(modelOperation, model);
+            Controller.Execute(modelOperation, model);
         }
 
         public void Dispose()
